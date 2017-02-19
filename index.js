f10b7b4171e8c1a6c4308446388d33b8b9490e1e
Emanuel Kluge <emanuel.kluge@jimdo.com>
2017-02-19 16:13:47 +0100

    Implement extraction of IPTC data

--- index.js
+++ index.js
@@ -4,12 +4,20 @@
  * Author Emanuel Kluge (http://emanuel-kluge.de/)
 */
 
+const fs = require('fs');
 const evaluate = require('node-eval');
 const ExifImage = require('exif').ExifImage;
+const iptc = require('node-iptc');
 
 const NO_EXIF_SEGMENT = 'NO_EXIF_SEGMENT';
 
-const getFile = (publicPath, content) => {
+const isError = err =>
+    err && err.code !== NO_EXIF_SEGMENT;
+
+const hasNoExifData = err =>
+    err && err.code === NO_EXIF_SEGMENT;
+
+const getFile = (publicPath, content) => Promise.resolve((() => {
     try {
         return {
             file: evaluate(`__webpack_public_path__='${publicPath}'; ${content}`),
@@ -17,13 +25,30 @@ const getFile = (publicPath, content) => {
     } catch (e) {
         return {};
     }
+})());
+
+const getExifData = image => new Promise((resolve, reject) => {
+    const extractor = new ExifImage({ image }, (err, data) => {
+        if (isError(err)) {
+            return reject(err);
+        }
+        const exif = hasNoExifData(err) ? extractor.exifData : data;
+        return resolve({ exif });
+    });
+});
+
+const getIptcData = image => new Promise((resolve, reject) =>
+    fs.readFile(image, (err, data) => {
+        if (err) {
+            return reject(err);
+        }
+        return resolve({ iptc: iptc(data) || {} });
+    }));
+
+const mergeResults = done => (results) => {
+    const merged = results.reduce((acc, item) => Object.assign({}, acc, item), {});
+    return done(null, `module.exports = ${JSON.stringify(merged)};`);
 };
-
-const isError = err =>
-    err && err.code !== NO_EXIF_SEGMENT;
-
-const hasNoExifData = err =>
-    err && err.code === NO_EXIF_SEGMENT;
 
 module.exports = function exifLoader(content) {
     if (this.cacheable) {
@@ -33,13 +58,12 @@ module.exports = function exifLoader(content) {
     /* eslint "no-underscore-dangle": 0 */
     const publicPath = this._compilation.outputOptions.publicPath || '/';
     /* eslint "no-underscore-dangle": 1 */
-    const file = getFile(publicPath, content);
-    const extractor = new ExifImage({ image: this.resourcePath }, (err, data) => {
-        if (isError(err)) {
-            return done(err);
-        }
-        const exif = hasNoExifData(err) ? extractor.exifData : data;
-        const result = Object.assign({ exif }, file);
-        return done(null, `module.exports = ${JSON.stringify(result)}`);
-    });
+    Promise
+        .all([
+            getFile(publicPath, content),
+            getExifData(this.resourcePath),
+            getIptcData(this.resourcePath),
+        ])
+        .then(mergeResults(done))
+        .catch(done);
 };
